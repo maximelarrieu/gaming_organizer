01964f2a10ef3dbe5609850ab377bdfe8a31a13f
Maxime Larrieu <LARRIEU Maxime>
2021-01-27 12:07:10 +0100

    :sparkles: autogenerate migration to create user table in database

---
+++ database/migrations/20210127092754-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: {
+          msg: "Le nom d'utilisateur est déjà utilisé. Veuillez en saisir un nouveau."
+        }
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: {
+          msg: "Cette adresse email est déjà utilisée. Veuillez en saisir une différente."
+        }
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};

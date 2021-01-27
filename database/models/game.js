601f03786150f33251a74163d0eb0b88c2541b65
Maxime Larrieu <LARRIEU Maxime>
2021-01-27 12:05:20 +0100

    :sparkles: create game model

---
+++ database/models/game.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Game extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Game.init({
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING,
+    releasedAt: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Game',
+  });
+  return Game;
+};

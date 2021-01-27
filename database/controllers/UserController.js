cda63c58a3ebe01a464fcfc940ccc4d2b2193e18
Maxime Larrieu <LARRIEU Maxime>
2021-01-27 12:12:14 +0100

    :sparkles: create user controller and findOne function which search in database and return data to try connection form

---
+++ database/controllers/UserController.js
@@ -0,0 +1,45 @@
+const db = require("../models")
+const User = db.User;
+const Op = db.Sequelize.Op;
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const privateKey = require('../auth/private_key')
+
+exports.findOne = (req, res) => {
+    const username = req.query.username;
+    const email = req.query.email;
+    const password = req.query.password;
+
+    User.findOne({where: { username: req.body.username, email: req.body.email, password: req.body.password}})
+    .then(user => {
+        if(!user) {
+            const message = "L'utilisateur n'existe pas."
+            return res.status(404).json({message})
+        }
+        const token = jwt.sign(
+            {userId: user.id},
+            privateKey,
+            {expiresIn: '24h'}
+        )
+        bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+            if(isPasswordValid) {
+                res.send(user)
+                const message = "L'utilisateur a été connecté avec succès";
+                return res.json({message, data:user, token})
+            } else {
+                const message = "Le mot de passe est incorrect."
+                return res.status(404).json({message})
+            }
+        })
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "ERROR"
+        });
+    });
+}
+
+exports.test = (req, res) => {
+
+}

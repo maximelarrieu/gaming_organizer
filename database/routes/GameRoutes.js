4ae751d9287c17ec93b79f57255c80893eaa0980
Maxime Larrieu <LARRIEU Maxime>
2021-02-06 18:23:31 +0100

    :wrench: cors config for routes

--- database/routes/GameRoutes.js
+++ database/routes/GameRoutes.js
@@ -1,11 +1,16 @@
 const auth = require('../auth/auth_middleware')
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 module.exports = app => {
     const games = require("../controllers/GameController");
 
     let router = require('express').Router();
 
-    router.get("/games", games.findAll);
+    router.get("/games", cors(corsOptions), games.findAll);
 
-    app.use('/api/games', auth, router)
+    app.use('/api', router)
 }

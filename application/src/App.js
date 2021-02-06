c8e8afd12554e541bb6f1dd2dce949134c8bcd2f
Maxime Larrieu <LARRIEU Maxime>
2021-02-06 20:14:09 +0100

    :sparkles: react router dom implementation for all routes

--- application/src/App.js
+++ application/src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './components/Home';
 import GameList from './components/GameList';
+import GameDetails from './components/GameDetails';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
+        <Route path="/games/:id" component={GameDetails} />
         <Route path="/games" component={GameList} />
         <Route path="/" component={Home} />
       </Switch>

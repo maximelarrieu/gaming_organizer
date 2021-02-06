04c4e3d31fe347e054ff89861be63245220113ca
Maxime Larrieu <LARRIEU Maxime>
2021-02-06 20:17:24 +0100

    :sparkles: navbar component

---
+++ application/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import '../styles/All.css'
+import '../styles/NavBar.css'
+
+const NavBar = () => (
+    <AppBar position="static" style={{backgroundColor: "#282c34"}}>
+        <Toolbar>
+            <Button>
+                <a href="/" className='home-button'>
+                    <img src="/logo.png" alt="gaming organizer"/>
+                </a>
+            </Button>
+            <Button>
+                <a href="/games" className='others-button'>MES JEUX</a>
+            </Button>
+        </Toolbar>
+    </AppBar>
+)
+
+export default NavBar

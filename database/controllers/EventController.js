45cd58149b54659b9249eeeb725e5c62aed715d8
Maxime Larrieu <LARRIEU Maxime>
2021-02-10 21:56:54 +0100

    :sparkles: event controller to create function for api

--- database/controllers/EventController.js
+++ database/controllers/EventController.js
@@ -1,23 +1,60 @@
 const db = require("../models");
 const Event = db.Event;
+const Game = db.Game;
 const Op = db.Sequelize.Op;
 const auth = require('../auth/auth_middleware')
 
 exports.findAll = (req, res) => {
-    const title = req.query.title;
-    let condition = title ? { title: { [Op.like]: `%${title}`}} : null;
 
-    Event.findAll()
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "ERROR"
-            });
+    const event = {
+        title: req.body.title,
+        description: req.body.description,
+        players: req.body.players,
+        startedAt: req.body.startedAt
+    };
+    Event.create = (req, res) => {
+        Event.create(event)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    Event.findAll({
+        include: [
+            {
+                model: Game
+            }
+        ]
+    })
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "ERROR"
         });
+    })
 };
+
+// exports.findAllByGames = (req, res) => {
+//     const title = req.query.title;
+//     let condition = game_id ? { game_id: { [Op.eq]: Game.id}} : null;
+
+//     Event.findAll({where: condition })
+//         .then(data => {
+//             res.send(data)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+// }
+
+Event.belongsTo(Game, {foreignKey: 'game_id'})
+
 
 // exports.findOne = (req, res) => {
 //     const id = req.params.id

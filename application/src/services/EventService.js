66632727574aea5c15b4263e57b343d162921f43
Maxime Larrieu <LARRIEU Maxime>
2021-02-10 21:56:29 +0100

    :sparkles: axios route for events model

--- application/src/services/EventService.js
+++ application/src/services/EventService.js
@@ -4,6 +4,14 @@ class EventService {
     findAll() {
         return http.get('/events')
     }
+
+    create(data) {
+        return http.post("/events", data)
+    }
+
+    // findAllByGames() {
+    //     return http.get('/games/:id')
+    // }
 }
 
 export default new EventService();

b70780ea7ce32c5e5327b82b99085c1c9cffedf7
Maxime Larrieu <LARRIEU Maxime>
2021-02-10 21:58:01 +0100

    :zap: findOne route for games

--- database/routes/GameRoutes.js
+++ database/routes/GameRoutes.js
@@ -1,5 +1,6 @@
 const auth = require('../auth/auth_middleware')
 const cors = require('cors')
+
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204

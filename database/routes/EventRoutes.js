2cf71f20c42cb81dc3a98392590011f5d3d1e21f
Maxime Larrieu <LARRIEU Maxime>
2021-02-10 21:57:34 +0100

    :sparkles: first event routes

--- database/routes/EventRoutes.js
+++ database/routes/EventRoutes.js
@@ -1,5 +1,8 @@
 const auth = require('../auth/auth_middleware')
 const cors = require('cors')
+
+const Game = require('../models/game')
+
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -11,6 +14,8 @@ module.exports = app => {
     let router = require('express').Router();
 
     router.get("/events", cors(corsOptions), events.findAll);
+
+    router.post("/events/:game_id/create", cors(corsOptions), events.create)
 
     app.use('/api', router)
 }

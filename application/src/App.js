cce732a40ff799b9aa2f1c52b9dd5b4d6e08dc3e
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-12 16:41:32 +0100

    :zap: change routes order

--- application/src/App.js
+++ application/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 import GameList from './components/GameList';
 import GameDetails from './components/GameDetails';
 import EventList from './components/EventList';
-import NavBar from './components/NavBar';
 import EventDetails from './components/EventDetails';
+import EventCreate from './components/EventCreate';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Switch>
-        <Route path="/events/:id" component={EventDetails} />
-        <Route path="/events" component={EventList} />
-        <Route path="/games/:id" component={GameDetails} />
-        <Route path="/games" component={GameList} />
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/games" component={GameList} />
+        <Route exact path="/games/:id" component={GameDetails} />
+        <Route exact path="/events" component={EventList} />
+        <Route exact path="/events/create" component={EventCreate} />
+        <Route exact path="/events/:id" component={EventDetails} />
       </Switch>
     </Router>
   );

f6c8917b2439a017c1833b414711c7ef7ac8dc1c
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-12 16:44:25 +0100

    :zap: improve cors options & security

--- database/server.js
+++ database/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan')
 const app = express();
 const port = 3001;
 
+var corsOptions = {
+   origin: "http://localhost:3001"
+ };
+
 const sequelize = new Sequelize('mysql://root:root@127.0.0.1:3306/gaming_organizer');
+
+// app.use(cors(corsOptions));
 
 sequelize.authenticate()
     .then(() => {
@@ -14,23 +22,25 @@ sequelize.authenticate()
        console.error('Unable to connect to the database: ', err)
     })
 
+app.use(morgan("combined"));
 app.get('/', (req, res) => {
-   res.send("SERVER");
+   res.json("SERVER");
    console.log('app reçue?');
 });
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 require("./routes/GameRoutes")(app);
 require("./routes/EventRoutes")(app);
 
-app.use(cors());
-app.options('*', cors());
+
 app.listen(port, () => console.log(`Port sur écoute : ${port}`));
 const db = require('./models');
 db.sequelize.sync();

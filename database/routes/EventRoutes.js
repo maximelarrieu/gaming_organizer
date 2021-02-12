5fa559f6e369439f8032ae4c9a9397730a54c587
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-12 16:44:07 +0100

    :sparkles: new create route

--- database/routes/EventRoutes.js
+++ database/routes/EventRoutes.js
@@ -1,5 +1,6 @@
 const auth = require('../auth/auth_middleware')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const Game = require('../models/game')
 
@@ -7,6 +8,12 @@ const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = app => {
     const events = require("../controllers/EventController");
@@ -17,7 +24,7 @@ module.exports = app => {
 
     router.get("/events/:id", cors(corsOptions), events.findOne);
 
-    // router.post("/events/:game_id/create", cors(corsOptions), events.create)
+    router.post("/events/create", cors(corsOptions), jsonParser, events.create)
 
     app.use('/api', router)
 }

4ce577f547750819c642a2cb8329099228a6cd2d
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-03 17:41:36 +0100

    :wrench: change cors access control

--- database/server.js
+++ database/server.js
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
